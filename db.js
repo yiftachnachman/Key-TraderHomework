2ac1c2cc503668fe88c9cf157042b67d9a9686cc
yiftachnachman <nachmanyiftach@gmail.com>
2020-02-24 19:31:55 -0800

    Justin this is for you

--- db.js
+++ db.js
@@ -1,23 +1,66 @@
+  
 var mariadb = require('mariadb');
-var async = require('async');
-var PRODUCTION_DB = 'codeVidTool';
-var TEST_DB = 'test_codeVidTool';
+
+
 exports.MODE_TEST = 'mode_test';
 exports.MODE_PRODUCTION = 'mode_production';
+
 var state = {
     pool: null,
     mode: null
 };
+
 exports.connect = function (mode, done) {
     state.pool = mariadb.createPool({
-        host: 'localhost',
+        socketPath: '/var/run/mysqld/mysqld.sock',
         user: 'root',
         password: '1234',
-        database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
+        database: 'KeyTrader'
     });
+
     state.mode = mode;
     done();
 };
+
 exports.get = function () {
     return state.pool;
 };
+
+exports.getUsers = function (successCb, errCb) {
+    var sql = "SELECT * FROM Users;";
+    this.get().query(sql).then(successCb).catch(errCb);
+}
+
+exports.addPost = function (postToInsert, successCb, errCb) {
+    var sql = "INSERT INTO posts(title, content) values(:title,:content);";
+
+    this.get()
+        .query(
+            {namedPlaceholders: true, sql:sql},
+            postToInsert
+        )
+        .then(successCb)
+        .catch(errCb);
+}
+
+exports.getAllPosts = function (successCb, errCb) {
+    var sql = "SELECT * FROM posts;";
+
+    this.get()
+        .query(
+            {sql: sql}
+        )
+        .then(successCb)
+        .catch(errCb);
+}
+
+exports.deletePost = function (idToDelete, successCb, errCb) {
+    var sql = "DELETE FROM posts WHERE post_id = ?;";
+    this.get()
+        .query(
+            sql,
+            [idToDelete]
+        )
+        .then(successCb)
+        .catch(errCb);
+}
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,44 @@ var router = express.Router();
 var db = require('../db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+/* CREATE (Post)*/
+var mariadb = "INSERT INTO tableName (column_1, column_2, ... ) VALUES (value1, value2, ... );";   
+
+/* READ (Get)*/
+var mariadb = "SELECT * FROM ??; ";
+var inserts = ['Users'];
+mariadb = maria.format(mariadb, inserts);
+db.get().query(mariadb, function(error, result) {
+  if (error){
+    callback(error);
+  }
+  console.log("POST: " + JSON.stringify(result));
+}
+/* UPDATE (Patch) */
+
+/* DELETE (Delete) */
+
+/* Example for help
 var sql = "SELECT * FROM ?? WHERE ??=?;";
 var inserts = ['users', 'username', username];
 sql = mysql.format(sql, inserts);
 db.get().query(sql, function (error, result) {
-if (error) {
-callback(error);
-}
-console.log("POST(select): " + JSON.stringify(result));
-if (result.length !== 0) {
+  if (error) {
+    callback(error);
+  }
+  console.log("POST(select): " + JSON.stringify(result));
+  if (result.length !== 0) {
     userId = result[0].id;
-}
-else {
+  }
+  else {
     userId = -1;
-}
-callback(null, 0);
+  }
+  callback(null, 0);
 });
+*/
